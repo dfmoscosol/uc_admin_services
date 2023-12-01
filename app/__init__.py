b3a9f5337b59677f98500ceb82bd3527114451b3
dfmoscosol <110931339+dfmoscosol@users.noreply.github.com>
2023-12-01 12:07:50 -0500

    Update CORS

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,12 @@ from datetime import timedelta
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app,resources={r"/*/*":{"origins":"*"},})
 app.config.from_object(Config)
 app.config['JWT_SECRET_KEY'] = 'innovacion2024'  
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1) 
 jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-# Cualquier origen (dominio) puede acceder a los recursos de la aplicación.
-CORS(app,resources={r"/*/*":{"origins":"*"},})
 
 from app import routes, models
